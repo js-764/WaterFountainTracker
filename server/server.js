4f3df46c5916b92806584c45d0ae3b7831114c2c
Jesse Slappy <jrs625@drexel.edu>
2025-09-04 12:57:43 -0400

    Fixed get/put requests for the frontend

--- server/server.js
+++ server/server.js
@@ -9,7 +9,9 @@ const {Client} = require('pg')
 //All the cors stuff to connect to the frontend
 const cors = require("cors")
 const corsOptions = {
-    origin: ["http://localhost:5173"]
+    origin: [
+        "http://localhost:5173"
+    ]
 }
 
 app.use(cors(corsOptions))
